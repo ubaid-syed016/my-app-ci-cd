c3bdcf1d2d588bd46345941e13de400bdf7c70bd
Ubaidullah <154274264+ubaid-syed016@users.noreply.github.com>
2026-01-20 18:43:27 +0500

    Update greeting message in index.js

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hi my name is Ubaidullah!'));
+app.get('/', (req, res) => res.send('Hi my name is Ubaidullah Syed!'));
 
 app.listen(port, () => console.log(`App running on port ${port}`));
 
